c4f08aa5d90fac5765dcc7f8a9bb14f08dcbc11c
GOLF <pakin.n@muze.co.th>
2025-02-17 21:01:13 +0700

    fix: apply auth guard in upload image route

--- src/upload/upload-image.controller.ts
+++ src/upload/upload-image.controller.ts
@@ -2,16 +2,19 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadImageService } from './upload-image.service';
 import { FileUploadInterceptor } from 'src/interceptors/file-upload.interceptor';
+import { JwtAuthGuard } from 'src/guard/jwt-auth.guard';
 
 @Controller('upload')
 export class UploadImageController {
   constructor(private readonly cloudinaryService: UploadImageService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   @UseInterceptors(FileUploadInterceptor.getInterceptor())
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
